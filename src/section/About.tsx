5fb4fb2a95281ecc6a995bef80219d1455a613e8
Firos Malik Abdillah <144033250+Firosmalik000@users.noreply.github.com>
2025-04-20 07:27:30 +0700

    Update About.tsx

--- src/section/About.tsx
+++ src/section/About.tsx
@@ -9,13 +9,13 @@ const About = () => {
 
   const content = {
     en: {
-      title: 'About Us',
-      tag: 'Where Innovation Meets Energy Excellence.',
+      title: 'ABOUT US',
+      tag: 'Where Innovation Meets Energy Excellence',
       description: `PT. HADIRA MINERGI NUSANTARA is a distinguished energy company founded by seasoned professionals with decades of expertise in the oil, gas, and mining sectors. Guided by the combined scope of KLBI 06100 and KLBI 06201, the company is actively engaged in the exploration, extraction, processing, and distribution of both crude oil and natural gas resources. With operations rooted in innovation and sustainability, PT. HADIRA MINERGI NUSANTARA aspires to play a pivotal role in meeting Indonesia's growing energy demands while driving economic growth.`,
     },
     id: {
-      title: 'Tentang Kami',
-      tag: 'Ketika Inovasi Bertemu Keunggulan Energi.',
+      title: 'TENTANG KAMI',
+      tag: 'Ketika Inovasi Bertemu Keunggulan Energi',
       description: `PT. HADIRA MINERGI NUSANTARA merupakan perusahaan energi terkemuka yang didirikan oleh para profesional berpengalaman dengan pengalaman puluhan tahun di sektor minyak, gas, dan pertambangan. Dipandu oleh cakupan gabungan KLBI 06100 dan KLBI 06201, perusahaan ini secara aktif terlibat dalam eksplorasi, ekstraksi, pemrosesan, dan distribusi sumber daya minyak mentah dan gas alam. Dengan operasi yang berakar pada inovasi dan keberlanjutan, PT. HADIRA MINERGI NUSANTARA bercita-cita untuk memainkan peran penting dalam memenuhi permintaan energi Indonesia yang terus meningkat sekaligus mendorong pertumbuhan ekonomi.`,
     },
   };
@@ -32,15 +32,15 @@ const About = () => {
 
         {/* Text - 40% */}
         <div className="w-full md:w-[40%] text-gray-800">
-          <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-3xl md:text-4xl font-playfair font-bold mb-4 md:text-left text-center">
+          <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-3xl md:text-4xl font-playfair font-bold mb-4  text-center">
             {content[language].title}
           </motion.h2>
 
-          <motion.h3 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-xl md:text-2xl font-semibold mb-6 md:text-left text-center">
+          <motion.h3 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-xl md:text-2xl font-semibold mb-6  text-center">
             {content[language].tag}
           </motion.h3>
 
-          <motion.p whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-base md:text-lg leading-relaxed text-justify indent-8">
+          <motion.p whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -30 }} transition={{ duration: 0.8 }} className="text-sm md:text-lg leading-relaxed text-justify indent-8">
             {content[language].description}
           </motion.p>
         </div>
