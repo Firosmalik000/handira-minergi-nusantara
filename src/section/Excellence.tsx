eecbd413fe20f037706317a96e0932934239f5c7
Firos Malik Abdillah <144033250+Firosmalik000@users.noreply.github.com>
2025-04-20 07:28:56 +0700

    Update Excellence.tsx

--- src/section/Excellence.tsx
+++ src/section/Excellence.tsx
@@ -9,7 +9,7 @@ const Excellence = () => {
 
   const excellenceContent = {
     en: {
-      title: 'Commitment to Excellence',
+      title: 'COMMITMENT TO EXCELLENCE',
       subtitle: 'PT. HADIRA MINERGI NUSANTARA upholds the highest standards in the following pillars:',
       highlights: [
         {
@@ -31,7 +31,7 @@ const Excellence = () => {
       mission: 'We are dedicated to delivering reliable, innovative, and sustainable energy solutions to empower industries, enhance livelihoods, and contribute to Indonesia’s future.',
     },
     id: {
-      title: 'Komitmen terhadap Keunggulan',
+      title: 'KOMITMEN TERHADAP KEUNGGULAN',
       subtitle: 'PT. HADIRA MINERGI NUSANTARA menjunjung standar tertinggi dalam pilar-pilar berikut:',
       highlights: [
         {
@@ -58,22 +58,22 @@ const Excellence = () => {
 
   return (
     <section id="excellence" className="w-full py-20 px-6 md:px-24 bg-orange-400 ">
-      <div className="text-center mb-16">
+      <div className="text-center mb-8">
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.2 }}
           transition={{ delay: 0.2, duration: 0.6 }}
-          className="text-4xl md:text-5xl font-bold text-white font-playfair mb-4"
+          className="text-4xl font-bold text-white font-playfair mb-4"
         >
           {content.title}
         </motion.h2>
-        <motion.p initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.2 }} transition={{ delay: 0.5, duration: 0.6 }} className="text-lg text-white max-w-3xl mx-auto">
+        <motion.p initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.2 }} transition={{ delay: 0.5, duration: 0.6 }} className="text-base md:text-lg text-white max-w-3xl mx-auto">
           {content.subtitle}
         </motion.p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mb-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mb-8">
         {content.highlights.map((item, index) => (
           <motion.div
             key={index}
@@ -81,7 +81,7 @@ const Excellence = () => {
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.2 }}
             transition={{ delay: index * 0.2, duration: 0.6 }}
-            className="bg-white rounded-2xl shadow-lg p-6 text-center hover:scale-105 transition duration-300"
+            className="bg-white rounded-2xl shadow-lg py-6 px-2 text-center hover:scale-105 transition duration-300"
           >
             <div className="flex justify-center">{item.icon}</div>
             <h3 className="text-xl font-semibold text-gray-800 mb-2 font-playfair">{item.title}</h3>
